e7f904206ff60dbaaa2f01927f1f3b5baf942c56
AlRizzaAPT <136303419+AlRizzaAPT@users.noreply.github.com>
2024-03-03 21:22:53 +0700

    Update Node.java

--- Pertemuan4/Node.java
+++ Pertemuan4/Node.java
@@ -4,25 +4,20 @@ public class Node {
 
 	private double data;
 	Node next;
-
+	
 	public Node (double data) {
 		this.data = data;
 	}
-
 	public double getData() {
 		return data;
 	}
-	
 	public void setData(double data) {
 		this.data = data;
 	}
-	
 	public Node getNext() {
 		return next;
 	}
-	
 	public void setNext(Node next) {
 		this.next = next;
 	}
-	
 }
