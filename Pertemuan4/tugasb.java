28997bfa5c81bc7effec9f6cc7ae99a8e2f2f87e
AlRizzaAPT <136303419+AlRizzaAPT@users.noreply.github.com>
2024-03-03 21:45:45 +0700

    Add files via upload

---
+++ Pertemuan4/tugasb.java
@@ -0,0 +1,17 @@
+package Pertemuan4;
+
+public class tugasb {
+
+		    public static void main(String[] args) {
+		    	 StrukturList list = new StrukturList(null);
+			        list.addHead(2.1);
+			        list.addHead(3.4);
+			        list.addMid(1.1, 3);
+			        list.addTail(4.5);
+			        list.addTail(5.5);
+			        list.displayElement();
+
+		        System.out.println();
+	}
+
+}
